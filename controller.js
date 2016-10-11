dcb77684ecad416c7d70f84781be2630187d4509
Sergiy <sergiy.ilchyshyn@gmail.com>
2016-10-11 15:25:47 +0300

    first change

--- controller.js
+++ controller.js
@@ -1,3 +1,9 @@
-function AppCtrl() {
-	console.log("Hello world from controller")
+function AppCtrl($scope, $http) {
+	console.log("Hello world from controller");
+
+$http.get('/contactlist').success(function(response) {
+	console.log("I got the data I require 1");
+	$scope.contactlist = response;
+});
+
 }
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
+
+app.get('/contactlist', function(req, res) {
+	console.log("I received a GET request 2")
+
+	person1 = {
+		name: 'Tom',
+		email: 'tom@email1.com',
+		number: '(111) 111-1111'
+	};
+
+	var contactlist = [person1];
+	res.json(contactlist);
+});
 
 app.listen(3000);
 console.log("Server running on port 3000");
